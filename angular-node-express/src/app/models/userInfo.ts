3b4bb741bcc1818a093178ee7c00bce54e32c2ab
adaboina21 <adaboina@gmail.com>
2019-12-05 15:12:34 -0800

    initial-checkin
    initiali-checkin

---
+++ angular-node-express/src/app/models/userInfo.ts
@@ -0,0 +1,20 @@
+export class UserInfoModel
+{
+	guid: string;
+	uid: string;
+	
+	first_name: string;
+	last_name: string;
+
+	email: string;
+	zipcode: string;
+
+
+	constructor(obj: any = null)
+	{
+		if(obj != null)
+		{
+			Object.assign(this, obj);
+		}
+	}
+}
